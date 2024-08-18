7e9a7cc8f8ddcce36c3fca554e485532143c1753
Oliver <oliver.markarian@gmail.com>
2024-08-18 19:56:15 +0200

    Climb ladder implementation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector2 moveinput;
+    [SerializeField] float runSpeed = 11f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 10f;
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    CapsuleCollider2D myCapsuleCollider;
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Run();
+        FlipSprite();
+        ClimbLadder();
+    }
+    void OnMove(InputValue value)
+    {
+        moveinput = value.Get<Vector2>();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {return;}
+
+        if (value.isPressed)
+        {
+            myRigidBody.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
+    
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveinput.x * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+    }
+
+    
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+
+    }
+    void ClimbLadder(){
+
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) {return;}
+
+        Vector2 climbVelocity = new Vector2 (myRigidBody.velocity.x, moveinput.y * climbSpeed);
+        myRigidBody.velocity = climbVelocity;
     }
 }
